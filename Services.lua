6730eccba77168545bb5da8637f1e5b172b5943e
Ro-Chat <121181030+Ro-Chat@users.noreply.github.com>
2023-04-19 06:22:09 -0700

    Create Services.lua

---
+++ Services.lua
@@ -0,0 +1,20 @@
+local mt = getrawmetatable(getfenv(0))
+local old = mt.__index
+
+setreadonly(mt, false)
+
+mt.__index = function(self, property)
+    if not old.checkcaller() then 
+      return old[property]
+    end
+    
+    local value = old[property]
+
+    if not value then
+        return old.game:GetService(property)
+    end
+    
+    return value
+end
+
+setreadonly(mt, true)
